341f83ca89920b21c109ef002043a78ee11ffa00
Miao Yueh Su <bluesmy1020@gmail.com>
2020-02-15 16:38:32 +0800

    Add admin restaurant page

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -28,6 +28,12 @@ const adminController = {
         req.flash('success_messages', 'restaurant was successfully created')
         res.redirect('/admin/restaurants')
       })
+  },
+
+  getRestaurant: (req, res) => {
+    return Restaurant.findByPk(req.params.id).then(restaurant => {
+      return res.render('admin/restaurant', JSON.parse(JSON.stringify({ restaurant: restaurant })))
+    })
   }
 }
 module.exports = adminController
