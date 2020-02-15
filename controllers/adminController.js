cfc5e8f76133c1d2b472005ea47e6a8a9ddbab91
Miao Yueh Su <bluesmy1020@gmail.com>
2020-02-15 16:33:10 +0800

    Add admin restaurant post feature

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -4,12 +4,30 @@ const Restaurant = db.Restaurant
 const adminController = {
   getRestaurants: (req, res) => {
     return Restaurant.findAll().then(restaurants => {
-      return res.render('admin/restaurants', { restaurants: restaurants })
+      return res.render('admin/restaurants', JSON.parse(JSON.stringify({ restaurants: restaurants })))
     })
   },
 
   createRestaurant: (req, res) => {
     return res.render('admin/create')
+  },
+
+  postRestaurant: (req, res) => {
+    if (!req.body.name) {
+      req.flash('error_messages', "name didn't exist")
+      return res.redirect('back')
+    }
+    return Restaurant.create({
+      name: req.body.name,
+      tel: req.body.tel,
+      address: req.body.address,
+      opening_hours: req.body.opening_hours,
+      description: req.body.description
+    })
+      .then((restaurant) => {
+        req.flash('success_messages', 'restaurant was successfully created')
+        res.redirect('/admin/restaurants')
+      })
   }
 }
 module.exports = adminController
